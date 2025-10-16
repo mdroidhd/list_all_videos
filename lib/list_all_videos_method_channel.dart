8a361ae65fb7f0acecab2438911ef2bd0dc9fb07
mibrahim0001 <79988085+mdroidhd@users.noreply.github.com>
2025-10-16 20:21:52 +0200

    Actualiza las dependencias de device_info_plus y permission_handler a las versiones más recientes

--- lib/list_all_videos_method_channel.dart
+++ lib/list_all_videos_method_channel.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
+import 'list_all_videos_platform_interface.dart';
 
 /// An implementation of [ListAllVideosPlatform] that uses method channels.
-class MethodChannelListAllVideos {
+class MethodChannelListAllVideos extends ListAllVideosPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('list_all_videos');
 
+  @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
